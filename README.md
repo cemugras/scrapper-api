f1eb91f6c6fc2d235c1c1257115bf16c8b934b15
cemugras <cemugrs@gmail.com>
2024-03-10 02:37:43 +0300

    /api/weather GET development for weather data, cron development for fetching weather info to DB

--- README.md
+++ README.md
@@ -2,27 +2,34 @@
 
 This project was generated with [Node.js](https://nodejs.org/en) version 18.17.1.
 
-| Package   | Version     |
-|-----------|-------------|
-| express   | 4.18.2      |
-| Puppeteer | 22.3.0      |
+| Package      | Version  |
+|--------------|----------|
+| express      | 4.18.2   |
+| Puppeteer    | 22.3.0   |
+| aws-sdk      | 2.1571.0 |
+| node-cron    | 3.0.3    |
 
 - For express version -> `npm show express version`
 - For axios version -> `npm show puppeteer version`
+- For aws-sdk version -> `npm show aws-sdk version`
+- For aws-sdk version -> `npm show node-cron version`
 
 The project is on live with the following endpoint:
 
 ## Development server
 
-- `node weather.js` or `nodemon index.js` or `npm start` for dev server. Navigate to `http:,//localhost:3000/`.
+- `npm start` for dev server. Navigate to `http:,//localhost:3000/`.
 - The application will automatically reload if you change any of the source files.
 
 ## Code scaffolding
 
 - `npm init -y` to generate a new project in existing module.
+- `npm install -g nodemon` -> tool for updating code changes automaticly without restart server.
 - `npm install express` -> required framework.
 - `npm install puppeteer` -> nodejs client.
-- `npm install -g nodemon` -> tool for updating code changes automaticly without restart server.
+- `npm install node-cron` -> for cronjob processes.
+- `npm install @aws-sdk/client-dynamodb` -> for AWS SDKv3 DynamoDB client.
+- `npm install @aws-sdk/credential-providers` -> for AWS SDKv3 DynamoDB client credentials with json.
 
 ## Used Techs
 
@@ -32,18 +39,11 @@ The project is on live with the following endpoint:
 2) #### [puppeteer](https://pptr.dev/) Development Notes  |
     - Puppeteer provides HTTP client.
     - It provides html data parser with waiting for all parameters.
-
-## Known Issues
-`npm audit fix` -> 1 moderate severity vulnerability
-
-`npm audit fix --force` -> if above command not works
-
-`npm install --legacy-peer-deps` -> upstream dependency conflict
-
-## Deployment
-
-## Fixes For Deployment
+3) #### [AWS DynamoDB](https://docs.aws.amazon.com/amazondynamodb/latest/developerguide/Introduction.html) Development Notes |
+    - It provides NoSQL database service.
+4) #### [node-cron](https://www.npmjs.com/package//node-cron) Development Notes |
+    - It enables to execute cron jobs.
 
 ## Features Inprogress
-- [ ] Weather Api for General Purposes
-- [ ] Weather Api Query Feature with Cities and Districts
+- [x] Weather Api for General Purposes
+- [x] Weather Api Query with Cities
--- api/weather.js
+++ api/weather.js
@@ -1,22 +1,138 @@
 const express = require('express');
 const puppeteer = require('puppeteer');
+const CityList = require('./CityList');
+const cron = require('node-cron');
 
 const app = express();
 const port = 3000;
 
+const {fromEnv} = require("@aws-sdk/credential-providers");
+const {DynamoDBClient, PutItemCommand, QueryCommand, DeleteItemCommand} = require('@aws-sdk/client-dynamodb');
+const dynamoDBClient = new DynamoDBClient({
+    region: "eu-north-1",
+    credentials: fromEnv(),
+});
+const tableName = 'weather_turkey';
+
 app.get('/api/weather', async (req, res) => {
     try {
-        console.log("req.params.city ", req.query.city);
-        const data = await getDataFromHtml('https://www.mgm.gov.tr/tahmin/il-ve-ilceler.aspx?il=' + req.query.city);
-        console.log("data", data);
+        const requestedCity = req.query.city;
+
+        const cityModel = CityList.find(city => city.cityName === requestedCity);
+
+        const queryItemCommand = new QueryCommand({
+            TableName: tableName,
+            KeyConditionExpression: 'CityID = :cityId',
+            ExpressionAttributeValues: {
+                ':cityId': {N: cityModel.cityID.toString()},
+            },
+        });
+        const response = await dynamoDBClient.send(queryItemCommand);
+        const queryItems = response.Items;
+
+        const result = {
+            cityName: queryItems[0].CityName.S,
+            cityID: queryItems[0].CityID.N,
+            date: queryItems[0].currentDate.S,
+            weather: queryItems[0].currentWeather.S,
+            weatherUrl: queryItems[0].currentWeatherIconUrl.S,
+            temperatureSign: queryItems[0].temperatureSign.S,
+            humidity: queryItems[0].humidity.S
+        }
         res.json({
-            data
+            result
         });
     } catch (error) {
-        console.error('Hata:', error.message);
+        console.error('Error:', error.message);
         res.status(500).json({error: 'Internal server error.'});
     }
 });
+
+cron.schedule('*/6 * * * *', async () => {
+    try {
+
+        for (const city of CityList) {
+            const cityName = city.cityName;
+            const data = await getDataFromHtml('https://www.mgm.gov.tr/tahmin/il-ve-ilceler.aspx?il=' + cityName);
+
+            //console.log("data", data);
+
+            // Query data with cityID
+            const queryItemCommand = new QueryCommand({
+                TableName: tableName,
+                KeyConditionExpression: 'CityID = :cityId',
+                ExpressionAttributeValues: {
+                    ':cityId': {N: city.cityID.toString()},
+                },
+            });
+            const response = await dynamoDBClient.send(queryItemCommand);
+            const queryItems = response.Items;
+            console.log("[QueryDynamo] City= " + cityName + ", row count: " + queryItems.length);
+
+            if (queryItems.length > 0) {
+                const deleteItemCommand = new DeleteItemCommand({
+                    TableName: tableName,
+                    Key: {
+                        CityID: {N: city.cityID.toString()}
+                    }
+                });
+
+                dynamoDBClient.send(deleteItemCommand)
+                    .then(response => {
+                        console.log("[DeleteDynamo] Item deleted :: City=", cityName);
+
+                        const putItemCommand = new PutItemCommand({
+                            TableName: tableName,
+                            Item: {
+                                CityID: {N: city.cityID.toString()},
+                                CityName: {S: city.cityName.toString()},
+                                currentWeather: {S: data.currentWeather},
+                                temperatureSign: {S: data.temperatureSign},
+                                currentDate: {S: data.currentDate},
+                                currentWeatherIconUrl: {S: data.currentWeatherIconUrl},
+                                humidity: {S: data.humidity}
+                            },
+                        });
+
+                        return dynamoDBClient.send(putItemCommand);
+                    })
+                    .then(response => {
+                        console.log("[PutDynamo] Item inserted :: City=" + cityName);
+                        console.log("--------------------");
+                    })
+                    .catch(error => {
+                        console.error("Error DynamoDB :: City=", cityName, error);
+                        console.log("--------------------");
+                    });
+            } else {
+                const putItemCommand = new PutItemCommand({
+                    TableName: tableName,
+                    Item: {
+                        CityID: {N: city.cityID.toString()},
+                        CityName: {S: city.cityName.toString()},
+                        currentWeather: {S: data.currentWeather},
+                        temperatureSign: {S: data.temperatureSign},
+                        currentDate: {S: data.currentDate},
+                        currentWeatherIconUrl: {S: data.currentWeatherIconUrl},
+                        humidity: {S: data.humidity}
+                    },
+                });
+
+                dynamoDBClient.send(putItemCommand)
+                    .then(response => {
+                        console.log("[PutDynamo] Item inserted :: City=" + cityName);
+                        console.log("--------------------");
+                    })
+            }
+
+        }
+
+        res.status(200).json({status: 'Success.'});
+    } catch (error) {
+        console.error('Error:', error.message);
+        res.status(500).json({error: 'Internal server error.'});
+    }
+})
 
 async function getDataFromHtml(url) {
     const browser = await puppeteer.launch();
@@ -31,7 +147,8 @@ async function getDataFromHtml(url) {
 
         // Parse required values and set to parameters
         const currentWeather = await page.$eval('div.anlik-sicaklik-deger.ng-binding', el => el.textContent.trim());
-        const currentDate = await page.$eval('section > h2:nth-child(4) > span', el => el.textContent.trim());
+        let currentDate = await page.$eval('section > h2:nth-child(4) > span', el => el.textContent.trim());
+        currentDate = currentDate.replace(".", ":");
         let currentWeatherIconUrl = await page.$eval('div.anlik-sicaklik-havadurumu-ikonu > img', (img) => img.getAttribute('src'));
         currentWeatherIconUrl = currentWeatherIconUrl.replace("..", "https://www.mgm.gov.tr");
         const humidityValue = await page.$eval('div.anlik-nem-deger-kac.ng-binding', el => el.textContent.trim());
