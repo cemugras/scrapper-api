5e9cbe0765614d39494a92d776d021fc7d2df2d1
cemugras <cemugrs@gmail.com>
2024-03-29 00:15:46 +0300

    dynamodb insert logic and some refactoring

--- api/weather/updateWeather.js
+++ api/weather/updateWeather.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const axios = require('axios');
 const CityList = require("../data/CityList");
-const Days = require("../data/Days");
 
 const app = express();
 const port = process.env.PORT || 3002;
@@ -16,6 +15,16 @@ const dynamoDBClient = new DynamoDBClient({
 });
 
 const tableName = 'weather_turkey';
+
+const dateOptions = {
+    day: '2-digit',
+    month: '2-digit',
+    year: 'numeric',
+    hour: '2-digit',
+    minute: '2-digit',
+    hour12: false,
+    timeZone: 'Europe/Istanbul' // Zaman dilimi gerekirse buraya uygun bir değer girilebilir
+};
 
 app.get('/api/weather/updateWeather', async (req, res) => {
     console.log("[updateWeather] STARTED");
@@ -38,23 +47,46 @@ app.get('/api/weather/updateWeather', async (req, res) => {
             const apiUrl = 'https://weather.com/api/v1/p/redux-dal';
 
             let retries = 0;
-            while (retries < 3) {
-                console.log("[updateWeather] CityID="+city.cityID, "CityName="+city.cityName, "Retry="+retries);
+            while (retries < 3 && !fetched) {
                 retries++;
+                console.log("[updateWeather] CityID="+city.cityID, "CityName="+city.cityName, "Retry="+retries);
                 await axios.post(apiUrl, requestBody)
                     .then(response => {
                         const data = response.data.dal.getSunV3CurrentObservationsUrlConfig['geocode:'+geocode+';units:m'].data;
                         const temperature = data.temperature;
+                        const temperatureMax24Hour = data.temperatureMax24Hour;
+                        const temperatureMin24Hour = data.temperatureMin24Hour;
                         const humidity = data.relativeHumidity;
-                        const day = Days.find((day) => day.dayEng === data.dayOfWeek);
-                        const weatherDate = data.validTimeLocal;
-
-                        console.log('[updateWeather] temperature:', temperature);
+                        const weatherType = data.cloudCoverPhrase;
+                        const weatherDate = new Date(data.validTimeLocal).toLocaleString('tr-TR', dateOptions);
+                        const insertDate = new Date().toLocaleString('tr-TR', dateOptions);
+                        console.log('[updateWeather] temperature:'+ temperature + ', max24Hour:' + temperatureMax24Hour + ', min24Hour:' + temperatureMin24Hour);
                         console.log('[updateWeather] humidity:', humidity);
-                        console.log('[updateWeather] day:', day);
                         console.log('[updateWeather] weatherDate:', weatherDate);
-                        if(temperature && humidity && day && weatherDate) {
+                        console.log('[updateWeather] insertDate:', insertDate);
+                        if(temperature && humidity && weatherDate) {
                             fetched = true;
+
+                            const putItemCommand = new PutItemCommand({
+                                TableName: tableName,
+                                Item: {
+                                    CityID: {N: city.cityID.toString()},
+                                    CityName: {S: city.cityName.toString()},
+                                    currentWeather: {S: temperature.toString()},
+                                    weatherDate: {S: weatherDate.toString()},
+                                    weatherMax24Hour: {S: temperatureMax24Hour.toString()},
+                                    weatherMin24Hour: {S: temperatureMin24Hour.toString()},
+                                    weatherType: {S: weatherType.toString()},
+                                    //currentWeatherIconUrl: {S: data.currentWeatherIconUrl},
+                                    humidity: {S: humidity.toString()},
+                                    insertDate: {S: insertDate.toString()}
+                                },
+                            });
+
+                            dynamoDBClient.send(putItemCommand).then(response => {
+                                console.log("[cron] Item inserted :: City=" + city.cityName + " CityID=" + city.cityID);
+                                console.log('[updateWeather] .............');
+                            })
                         }
                     })
                     .catch(error => {
@@ -65,8 +97,6 @@ app.get('/api/weather/updateWeather', async (req, res) => {
                 }
             }
 
-
-            console.log('[updateWeather] .............');
             /*const date = new Date();
             const options = {
                 day: 'numeric',
